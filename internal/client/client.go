0813ae26184164dd3043dadafa4af8be0911aa63
lexusegorov <alexey.egorov.job@gmail.com>
2025-06-09 20:59:51 +0700

    feat: add client

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,4 +1,54 @@
 package client
 
-//TODO: method for send requests with params
-//TODO: vidno budet
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	client      http.Client
+	minInterval int
+	maxInterval int
+	factor      int
+}
+
+func New(minInterval, maxInterval, factor int) *Client {
+	return &Client{
+		client:      http.Client{},
+		minInterval: minInterval,
+		maxInterval: maxInterval,
+		factor:      factor,
+	}
+}
+
+func (c Client) Request(method, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c.doRetry(req)
+}
+
+func (c Client) doRetry(req *http.Request) (*http.Response, error) {
+	var res *http.Response
+	var err error
+
+	delay := c.minInterval
+
+	for {
+		nextDelay := c.minInterval * c.factor
+		res, err = c.client.Do(req)
+
+		if err == nil || nextDelay > c.maxInterval {
+			break
+		}
+
+		time.Sleep(time.Millisecond * time.Duration(delay))
+		delay = nextDelay
+	}
+
+	return res, err
+}
