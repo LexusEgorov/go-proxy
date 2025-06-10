fe542cc9081ead1d91b2b7dadbbe638bf9aaf371
lexusegorov <alexey.egorov.job@gmail.com>
2025-06-10 18:21:09 +0700

    feat: add server

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,14 +1,35 @@
 package main
 
-/*PART 1: net/http client + srv*/
-//TODO: config: retries count, requests interval, time for request
-//TODO: srv: 1 handler, which send method, uri + params, headers, body to client
-//TODO: client (net/http): requests with all data from srv to jsonplaceholder
-//TODO: dockerfile
-//TODO: makefile
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/LexusEgorov/go-proxy/internal/client"
+	"github.com/LexusEgorov/go-proxy/internal/config"
+	"github.com/LexusEgorov/go-proxy/internal/server"
+)
 
 /*PART 2: client on resty*/
 //TODO: just move client to resty
-//TODO: maybe something more
+//TODO: try TUI
 
-func main() {}
+func main() {
+	cfg, err := config.New()
+
+	if err != nil {
+		log.Fatalf("config error: %v", err)
+		return
+	}
+
+	proxyClient := client.New(cfg.Client)
+	proxyServer := server.New(&cfg.Server, proxyClient)
+
+	proxyServer.Run()
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt)
+
+	<-stopChan
+	proxyServer.Stop()
+}
